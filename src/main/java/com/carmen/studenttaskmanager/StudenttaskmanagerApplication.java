6f6ab57ee41abd3ba92fa127c5117d0f1ffe6d1b
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-16 11:13:36 +0300

    initial spring boot project

---
+++ src/main/java/com/carmen/studenttaskmanager/StudenttaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.carmen.studenttaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenttaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudenttaskmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carmen/studenttaskmanager/StudenttaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carmen.studenttaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenttaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
