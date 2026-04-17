4b0368e9a6905c318e71000243de605492ce9c01
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-17 17:04:37 +0300

    add global exception handler

--- src/main/java/com/carmen/studenttaskmanager/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/carmen/studenttaskmanager/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.carmen.studenttaskmanager.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneric(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong");
+    }
 }
